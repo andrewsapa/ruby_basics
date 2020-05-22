d8da135dc7da89bf6dae4b37dd65a2b10e60203a
Adrian Andrews <arnotaa@gmail.com>
2020-05-22 20:46:04 +0000

    add hashes exercises

---
+++ hashes/labeled_numbers.rb
@@ -0,0 +1,20 @@
+#Use Hash#each to iterate over numbers and print each element's key/value pair.
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+#-----------------------------------------
+
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+numbers.each do |k, v|
+    puts "A #{k} number is #{v}."
+end
